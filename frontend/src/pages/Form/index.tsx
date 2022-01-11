8f2a8a89b73e66ec308fdbdaf182e52c38a883a9
Renan Soares <renansoaresffc@gmail.com>
2022-01-11 18:14:23 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+function Form() {
+    return(
+        <h1>Formulário</h1>
+    );
+}
+
+export default Form;
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css';
+
+function Listing() {
+    return(
+        <h1>Lista de filmes</h1>
+    );
+}
+
+export default Listing;
